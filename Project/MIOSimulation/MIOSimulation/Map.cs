b00f7e7b7866b2d345f6ef303f226c2b7eab4909
Jose Ossorio <joseossorio99@hotmail.com>
2019-09-14 11:18:36 -0500

    set up project with basic map centered in Cali

---
+++ Project/MIOSimulation/MIOSimulation/Map.cs
@@ -0,0 +1,33 @@
+﻿using GMap.NET.WindowsForms;
+using System;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using GMap.NET;
+
+namespace MIOSimulation
+{
+    public partial class Map : Form
+    {
+
+        //Overlays to store markers and routes
+        private GMapOverlay markers = new GMapOverlay();
+        private GMapOverlay routes = new GMapOverlay();
+
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void Map_Load(object sender, EventArgs e)
+        {
+            //setting up the map
+            gmap.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            gmap.Position = new PointLatLng(3.442275, -76.515539);
+            gmap.ShowCenter = false;
+            gmap.Overlays.Add(markers);
+            gmap.Overlays.Add(routes);
+        }
+    }
+}
