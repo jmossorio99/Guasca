c24633b09975e99a8e36ef410b32dc51fc5eeeca
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-09-16 23:44:20 -0500

    added some interface tools: panel with backgound color and an icon for the application

--- Project/MIOSimulation/MIOSimulation/Map.Designer.cs
+++ Project/MIOSimulation/MIOSimulation/Map.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Map));
             this.gmap = new GMap.NET.WindowsForms.GMapControl();
             this.StationStop_CB = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gmap
@@ -41,7 +44,7 @@
             this.gmap.GrayScaleMode = false;
             this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gmap.LevelsKeepInMemmory = 5;
-            this.gmap.Location = new System.Drawing.Point(12, 25);
+            this.gmap.Location = new System.Drawing.Point(1, -2);
             this.gmap.MarkersEnabled = true;
             this.gmap.MaxZoom = 18;
             this.gmap.MinZoom = 13;
@@ -62,7 +65,7 @@
             // StationStop_CB
             // 
             this.StationStop_CB.FormattingEnabled = true;
-            this.StationStop_CB.Location = new System.Drawing.Point(989, 69);
+            this.StationStop_CB.Location = new System.Drawing.Point(75, 70);
             this.StationStop_CB.Name = "StationStop_CB";
             this.StationStop_CB.Size = new System.Drawing.Size(129, 21);
             this.StationStop_CB.TabIndex = 1;
@@ -71,25 +74,41 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1035, 53);
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(114, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(44, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Filtro";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.StationStop_CB);
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(893, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 495);
+            this.panel1.TabIndex = 3;
             // 
             // Map
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1189, 587);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.StationStop_CB);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1154, 493);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.gmap);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Map";
             this.Text = "Map";
             this.Load += new System.EventHandler(this.Map_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -98,5 +117,6 @@
         private GMap.NET.WindowsForms.GMapControl gmap;
         private System.Windows.Forms.ComboBox StationStop_CB;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
