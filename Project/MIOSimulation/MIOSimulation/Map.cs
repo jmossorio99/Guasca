eb7a388e4dac5fd208ab22ad2cc7f81266e31c1a
Jose Ossorio <joseossorio99@hotmail.com>
2019-09-16 23:30:08 -0500

    added method to add all the bus stops to the map. The method seems to work fine, but the data is too large and the map can't load the whole file. Will keep testing

--- Project/MIOSimulation/MIOSimulation/Map.cs
+++ Project/MIOSimulation/MIOSimulation/Map.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using GMap.NET.WindowsForms.Markers;
 using GMap.NET.MapProviders;
 using GMap.NET;
+using System.Collections.Generic;
 
 namespace MIOSimulation
 {
@@ -22,12 +23,33 @@ namespace MIOSimulation
         private void Map_Load(object sender, EventArgs e)
         {
             //setting up the map
+            markers.Markers.Add(new GMarkerGoogle(new PointLatLng(3.44327333, -76.54844833), GMarkerGoogleType.lightblue_pushpin));
+            markers.Markers.Add(new GMarkerGoogle(new PointLatLng(3.440465, -76.54748), GMarkerGoogleType.lightblue_pushpin));
+            //addAllStops();
             gmap.MapProvider = GoogleMapProvider.Instance;
             GMaps.Instance.Mode = AccessMode.ServerOnly;
             gmap.Position = new PointLatLng(3.442275, -76.515539);
             gmap.ShowCenter = false;
             gmap.Overlays.Add(markers);
             gmap.Overlays.Add(routes);
+            
+        }
+
+        private void addAllStops()
+        {
+            FileReader fr = new FileReader("stops.csv");
+            List<String> stopData = fr.readFile();
+            List<Double> coords = new List<Double>();
+            foreach (var item in stopData)
+            {
+                String[] tempSplit = item.Split(',');
+                coords.Add(Double.Parse(tempSplit[6]));
+                coords.Add(Double.Parse(tempSplit[5]));
+            }
+            for (int i = 0; i < coords.Count; i += 2)
+            {
+                markers.Markers.Add(new GMarkerGoogle(new PointLatLng(coords[i], coords[i + 1]), GMarkerGoogleType.lightblue_pushpin));
+            }
         }
     }
 }
