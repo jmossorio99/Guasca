741d635f953938542b1e32eae1b4508f05576154
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-09-29 21:05:29 -0500

    added method to draw a polygon and a auxiliary method to get center or a polygon and place a point with the station name

--- Project/MIOSimulation/MIOSimulation/Map.cs
+++ Project/MIOSimulation/MIOSimulation/Map.cs
@@ -14,6 +14,7 @@ namespace MIOSimulation
         //Overlays to store markers and routes
         private GMapOverlay markers = new GMapOverlay();
         private GMapOverlay routes = new GMapOverlay();
+        private int numberOfStations;
 
         public Map()
         {
@@ -33,6 +34,7 @@ namespace MIOSimulation
             StationStop_CB.Items.Add("Estaciones y paradas");
             StationStop_CB.Items.Add("Estaciones");
             StationStop_CB.Items.Add("Paradas");
+            numberOfStations = 0;
         }
 
         private void addAllStops()
@@ -44,6 +46,60 @@ namespace MIOSimulation
                 String[] tempSplit = item.Split(',');
                 markers.Markers.Add(new GMarkerGoogle(new PointLatLng(Double.Parse(tempSplit[7]), Double.Parse(tempSplit[6])), GMarkerGoogleType.blue_small));
             }
+
+            List<PointLatLng> points = new List<PointLatLng>();
+            PointLatLng p1 = new PointLatLng(3.41454278, -76.54957278);
+            PointLatLng p2 = new PointLatLng(3.41476833, -76.54962139);
+           // PointLatLng p3 = new PointLatLng(-76.89785083,3.487897899107);
+            points.Add(p1);
+            points.Add(p2);
+           // points.Add(p3);
+            paintPolygon(points,"Terminal cañaveralejo");
+
+        }
+
+        private void paintPolygon(List<PointLatLng> points, String stationName) {
+
+            GMapPolygon polygon = new GMapPolygon(points, stationName + " " + numberOfStations);
+            
+            GMapOverlay polygons = new GMapOverlay("station " + numberOfStations);
+            
+            polygons.Polygons.Add(polygon);
+
+            //update gmap
+
+            gmap.Overlays.Add(polygons);
+
+            PointLatLng center = getCenterPoint(points);
+            GMarkerGoogle marker1 = new GMarkerGoogle(center,GMarkerGoogleType.blue);
+            marker1.ToolTipText = ""+stationName;
+
+            markers.Markers.Add(marker1);
+            gmap.Overlays.Add(markers);
+
+            numberOfStations++;
+        }
+
+        private PointLatLng getCenterPoint(List<PointLatLng> vertexes) {
+
+            PointLatLng centerPoint = new PointLatLng();
+
+            int sum = 0;
+            double lat = 0;
+            double lng = 0;
+
+            foreach (var point in vertexes) {
+                sum++;
+                lat += point.Lat;
+                lng += point.Lng;
+            }
+
+            lat = lat / sum;
+            lng = lng / sum;
+            centerPoint.Lat = lat;
+            centerPoint.Lng = lng;
+
+            return centerPoint;
         }
     }
 }
