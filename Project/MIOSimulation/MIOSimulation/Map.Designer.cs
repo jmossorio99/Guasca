dd5778fa29968f803d0436b1274b36575ff92ba4
Your Name <you@example.com>
2019-09-29 22:53:09 -0500

    Simulacion de un dia de datagram

--- Project/MIOSimulation/MIOSimulation/Map.Designer.cs
+++ Project/MIOSimulation/MIOSimulation/Map.Designer.cs
@@ -28,16 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Map));
             this.gmap = new GMap.NET.WindowsForms.GMapControl();
             this.StationStop_CB = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.startSimulation = new System.Windows.Forms.Button();
+            this.prueba = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.goSimulation = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.faster = new System.Windows.Forms.Button();
+            this.slower = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            //
+            // 
             // gmap
-            //
+            // 
             this.gmap.Bearing = 0F;
             this.gmap.CanDragMap = true;
             this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
@@ -45,6 +54,7 @@
             this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gmap.LevelsKeepInMemmory = 5;
             this.gmap.Location = new System.Drawing.Point(1, -2);
+            this.gmap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gmap.MarkersEnabled = true;
             this.gmap.MaxZoom = 18;
             this.gmap.MinZoom = 0;
@@ -58,51 +68,141 @@
             this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gmap.ShowTileGridLines = false;
-            this.gmap.Size = new System.Drawing.Size(1188, 677);
+            this.gmap.Size = new System.Drawing.Size(1336, 846);
             this.gmap.TabIndex = 0;
             this.gmap.Zoom = 14D;
-            //
+            this.gmap.Load += new System.EventHandler(this.Gmap_Load);
+            // 
             // StationStop_CB
-            //
+            // 
             this.StationStop_CB.FormattingEnabled = true;
-            this.StationStop_CB.Location = new System.Drawing.Point(75, 70);
+            this.StationStop_CB.Location = new System.Drawing.Point(62, 63);
+            this.StationStop_CB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.StationStop_CB.Name = "StationStop_CB";
-            this.StationStop_CB.Size = new System.Drawing.Size(129, 21);
+            this.StationStop_CB.Size = new System.Drawing.Size(195, 28);
             this.StationStop_CB.TabIndex = 1;
             this.StationStop_CB.Text = "Estaciones y paradas";
-            //
+            this.StationStop_CB.SelectedIndexChanged += new System.EventHandler(this.StationStop_CB_SelectedIndexChanged);
+            // 
             // label1
-            //
+            // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(114, 47);
+            this.label1.Location = new System.Drawing.Point(121, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.Size = new System.Drawing.Size(69, 31);
             this.label1.TabIndex = 2;
             this.label1.Text = "Filtro";
-            //
+            // 
             // panel1
-            //
+            // 
             this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.slower);
+            this.panel1.Controls.Add(this.faster);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.goSimulation);
+            this.panel1.Controls.Add(this.prueba);
+            this.panel1.Controls.Add(this.startSimulation);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.StationStop_CB);
             this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.panel1.Location = new System.Drawing.Point(893, -2);
+            this.panel1.Location = new System.Drawing.Point(1005, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(261, 495);
+            this.panel1.Size = new System.Drawing.Size(294, 619);
             this.panel1.TabIndex = 3;
-            //
+            // 
+            // startSimulation
+            // 
+            this.startSimulation.ForeColor = System.Drawing.Color.Black;
+            this.startSimulation.Location = new System.Drawing.Point(110, 148);
+            this.startSimulation.Name = "startSimulation";
+            this.startSimulation.Size = new System.Drawing.Size(80, 30);
+            this.startSimulation.TabIndex = 3;
+            this.startSimulation.Text = "Simular movimiento";
+            this.startSimulation.UseVisualStyleBackColor = true;
+            this.startSimulation.Click += new System.EventHandler(this.StartSimulation_Click);
+            // 
+            // prueba
+            // 
+            this.prueba.AutoSize = true;
+            this.prueba.Location = new System.Drawing.Point(71, 201);
+            this.prueba.Name = "prueba";
+            this.prueba.Size = new System.Drawing.Size(163, 20);
+            this.prueba.TabIndex = 4;
+            this.prueba.Text = "Informacion de la ruta";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // goSimulation
+            // 
+            this.goSimulation.ForeColor = System.Drawing.Color.Black;
+            this.goSimulation.Location = new System.Drawing.Point(43, 246);
+            this.goSimulation.Name = "goSimulation";
+            this.goSimulation.Size = new System.Drawing.Size(93, 30);
+            this.goSimulation.TabIndex = 5;
+            this.goSimulation.Text = "Reanudar";
+            this.goSimulation.UseVisualStyleBackColor = true;
+            this.goSimulation.Click += new System.EventHandler(this.GoSimulation_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(168, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Pausar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // faster
+            // 
+            this.faster.ForeColor = System.Drawing.Color.Black;
+            this.faster.Location = new System.Drawing.Point(168, 294);
+            this.faster.Name = "faster";
+            this.faster.Size = new System.Drawing.Size(29, 26);
+            this.faster.TabIndex = 7;
+            this.faster.Text = "+";
+            this.faster.UseVisualStyleBackColor = true;
+            this.faster.Click += new System.EventHandler(this.Faster_Click);
+            // 
+            // slower
+            // 
+            this.slower.ForeColor = System.Drawing.Color.Black;
+            this.slower.Location = new System.Drawing.Point(205, 294);
+            this.slower.Name = "slower";
+            this.slower.Size = new System.Drawing.Size(29, 26);
+            this.slower.TabIndex = 8;
+            this.slower.Text = "-";
+            this.slower.UseVisualStyleBackColor = true;
+            this.slower.Click += new System.EventHandler(this.Slower_Click);
+            // 
             // Map
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1154, 493);
+            this.ClientSize = new System.Drawing.Size(1298, 616);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.gmap);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Map";
             this.Text = "Map";
             this.Load += new System.EventHandler(this.Map_Load);
@@ -118,5 +218,13 @@
         private System.Windows.Forms.ComboBox StationStop_CB;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button startSimulation;
+        private System.Windows.Forms.Label prueba;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button goSimulation;
+        private System.Windows.Forms.Button slower;
+        private System.Windows.Forms.Button faster;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Project/MIOSimulation/MIOSimulation/Map.cs
+++ Project/MIOSimulation/MIOSimulation/Map.cs
@@ -16,7 +16,9 @@ namespace MIOSimulation
         //Overlays to store markers and routes
         private GMapOverlay stops = new GMapOverlay("stops");
         private GMapOverlay stations = new GMapOverlay("stops");
-        private GMapOverlay routes = new GMapOverlay();
+        private GMapOverlay routes = new GMapOverlay("routes");
+        private List<String> dataSimulation = new List<string>();
+        private int number=0;
         private readonly string stationNamesFilePath = "./stationNames.txt";
         //private List<String> stationNames;
 
@@ -34,12 +36,14 @@ namespace MIOSimulation
 
             gmap.MapProvider = GoogleMapProvider.Instance;
             GMaps.Instance.Mode = AccessMode.ServerOnly;
-            gmap.Position = new PointLatLng(3.442275, -76.515539);
+            gmap.Position = new PointLatLng(3.428356, -76.510187);
             gmap.ShowCenter = false;
+            gmap.Zoom = 13;
 
-            gmap.Overlays.Add(stops);
-            gmap.Overlays.Add(stations);
-            gmap.Overlays.Add(routes);
+            //gmap.Overlays.Add(stops);
+           // gmap.Overlays.Add(stations);
+            //gmap.Overlays.Add(routes);
+            
 
             StationStop_CB.Items.Add("Estaciones y paradas");
             StationStop_CB.Items.Add("Estaciones");
@@ -78,6 +82,102 @@ namespace MIOSimulation
 
         }
 
+        private void Gmap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StationStop_CB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if(StationStop_CB.SelectedIndex == 0)
+            {
+                gmap.Overlays.Clear();
+                gmap.Overlays.Add(stops);
+                gmap.Overlays.Add(stations);
+                gmap.Zoom = 12.5;
+            }
+            else if(StationStop_CB.SelectedIndex == 2)
+            {
+                gmap.Overlays.Clear();
+                gmap.Overlays.Add(stops);
+                gmap.Zoom = 12.5;
+            }
+            else
+            {
+                gmap.Overlays.Clear();
+                gmap.Overlays.Add(stations);
+                gmap.Zoom = 12.5;
+            }
+        }
+
+        private void StartSimulation_Click(object sender, EventArgs e)
+        {
+            FileReader frUbication = new FileReader("datagramList.txt");
+            dataSimulation = frUbication.readFile();
+            timer1.Start();
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if(number< dataSimulation.Count)
+            {
+                gmap.Overlays.Clear();
+                routes.Markers.Clear();
+                String[] tempSplit = dataSimulation[number].Split(';');
+                GMapMarker marker;
+                Double lat1 = Double.Parse(tempSplit[4]);
+                Double lng1 = Double.Parse(tempSplit[3]);
+                marker = new GMarkerGoogle(new PointLatLng(lat1, lng1), new Bitmap("./img/station.png"));
+                if (Double.Parse(tempSplit[2]) != -1)
+                {
+                    marker.ToolTipText = "En ruta a las " + tempSplit[0];
+                    prueba.Text = "En ruta a las " + tempSplit[0];
+                }
+                else
+                {
+                    marker.ToolTipText = "Tiempo muerto a las " + tempSplit[0];
+                    prueba.Text= "Tiempo muerto a las " + tempSplit[0];
+                }
+                routes.Markers.Add(marker);
+                gmap.Overlays.Add(routes);
+                gmap.Position = new PointLatLng(lat1, lng1);
+                gmap.Zoom = 15;
+                number++;
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void GoSimulation_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Faster_Click(object sender, EventArgs e)
+        {
+            if (timer1.Interval > 50)
+            {
+                timer1.Interval = timer1.Interval - 50;
+            }
+        }
+
+        private void Slower_Click(object sender, EventArgs e)
+        {
+            if (timer1.Interval < 1000)
+            {
+                timer1.Interval = timer1.Interval+50;
+            }
+        }
+
         //private Boolean isStation(String name)
         //{
 
@@ -96,7 +196,7 @@ namespace MIOSimulation
         //            {
         //                return false;
         //            }
-                    
+
         //        }
         //    }
         //    return false;
