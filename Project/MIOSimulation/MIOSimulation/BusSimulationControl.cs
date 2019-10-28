d5ceb611b64b1d75d64123d8dd9a146db6d3f921
nelson quiñones <nelsonquinones2424@gmail.com>
2019-10-28 09:09:42 -0500

    Advances on n-BusSimulation

--- Project/MIOSimulation/MIOSimulation/BusSimulationControl.cs
+++ Project/MIOSimulation/MIOSimulation/BusSimulationControl.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GMap.NET;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +14,17 @@ namespace MIOSimulation
         private List<Bus> busesInSimulation;
         private long start;
         private long finish;
-        private Dictionary<String, int> dateReference;
+        private long offset;
+        private int movingTo;
+        private Dictionary<String, Bus> busReference;
+        private Dictionary<String, int> busMatch;
 
         public BusSimulationControl(long start, long finish) {
             this.start = start;
             this.finish = finish;
-            dateReference = new Dictionary<string, int>();
+            movingTo = -1;
+            busReference = new Dictionary<string, Bus>();
+            busMatch = new Dictionary<string, int>();
             busesInSimulation = new List<Bus>();
             timeLine = new List<List<BusLocation>>();
             InitializeSimulation();
@@ -29,32 +36,100 @@ namespace MIOSimulation
             FileReader br = new FileReader("busTravel");
             List<String> data = br.readFile();
             String lastDate = "";
-            int position = 0;
+            int position = -1;
             foreach (String line in data) {
                 string[] splitData = line.Split(';');
                 String date = splitData[0];
+                String busId = splitData[1];
                 String Lng = splitData[7];
                 String Lat = splitData[8];
+                PointLatLng location = new PointLatLng(Double.Parse(Lat, CultureInfo.InvariantCulture.NumberFormat), Double.Parse(Lng, CultureInfo.InvariantCulture.NumberFormat));
+                BusLocation temp = new BusLocation(location,busId);
+                if (!busReference.ContainsKey(busId)) {
+                    busReference.Add(busId, new Bus(busId, ""));
+                }
                 if (date.CompareTo(lastDate) == 0)
                 {
-                    
+                    timeLine[position].Add(temp);   
                 }
                 else {
-                    position++;
-                    timeLine.Add(new List<BusLocation>());
-                    timeLine[position].Add(new BusLocation());
+                    if (lastDate.Equals("")) { lastDate = date; offset = createNumber(lastDate); }
+                    long start = createNumber(lastDate);
+                    long finish = createNumber(date);
+                    for (long i = start+1; i <= finish; i++)
+                    {
+                        position++;
+                        timeLine.Add(new List<BusLocation>());
+                    }
+                    timeLine[position].Add(temp);
+                    lastDate = date;
                 }
             }
             
         }
 
-        public void setInterval(String start, String finish) {
+        private long createNumber(string date)
+        {
+            String[] data = date.Split(' ')[1].Split(':');
+            long result = long.Parse(data[0])*3600 + long.Parse(data[1])*60 + long.Parse(data[2]);
+            return result;
+        }
 
+        public void setInterval(String s, String f) {
+            start = createNumber(s)-offset;
+            finish = createNumber(f)-offset;
+            movingTo = (int)start;
         }
 
         public List<Bus> Next30() {
+            busesInSimulation = new List<Bus>();
+            int steps = 0;
+            while (movingTo <= finish && steps <= 30) {
+                foreach (BusLocation location in timeLine[movingTo]) {
+                    if (busReference.ContainsKey(location.BusName))
+                    {
+                        busReference[location.BusName].ActualPostion = location.Postion;
+                        busMatch.Add(location.BusName,1);
+                    }
+                }
+                movingTo++;
+                steps++;
+            }
+            checkNext30();
+            checkValid();
+            return busesInSimulation;
+        }
 
-            return null;
+
+        private void checkNext30()
+        {
+            int steps = 0;
+            while (movingTo <= finish && steps <= 30)
+            {
+                foreach (BusLocation location in timeLine[movingTo])
+                {
+                    if (busReference.ContainsKey(location.BusName))
+                    {
+                        busReference[location.BusName].NextPostion = location.Postion;
+                        if (busMatch.ContainsKey(location.BusName)) { busMatch.Add(location.BusName, 2); }
+                    }
+                }
+                movingTo++;
+                steps++;
+            }
+            movingTo -= steps;
+        }
+
+        private void checkValid()
+        {
+            foreach (var item in busMatch)
+            {
+                if (item.Value == 2)
+                {
+                    busesInSimulation.Add(busReference[item.Key]);
+                }
+            }
+
         }
     }
 }
