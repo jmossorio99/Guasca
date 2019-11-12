218ce601d47e22c50e95a5a341a7a36d3fd37e3f
Jose Ossorio <joseossorio99@hotmail.com>
2019-11-11 21:28:01 -0500

    now the iddle buses at a time t show in red instead of green

--- Project/MIOSimulation/MIOSimulation/BusLocation.cs
+++ Project/MIOSimulation/MIOSimulation/BusLocation.cs
@@ -12,17 +12,20 @@ namespace MIOSimulation
         private PointLatLng postion;
         private string busName;
         private int stopId;
+        private Boolean isIddle;
 
-        public BusLocation(PointLatLng postion, string busName,int stopId)
+        public BusLocation(PointLatLng postion, string busName,int stopId, Boolean isIddle)
         {
             Postion = postion;
             BusName = busName;
             StopId = stopId;
+            IsIddle = isIddle;
         }
 
         public PointLatLng Postion { get; set; }
         public string BusName { get; set; }
 
         public int StopId { get; set; }
+        public bool IsIddle { get; set; }
     }
 }
