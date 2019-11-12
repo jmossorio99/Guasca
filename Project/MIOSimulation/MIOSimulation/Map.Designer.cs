b30d905c21c5a92c8b0788bf6f07c693f8a52078
Your Name <you@example.com>
2019-11-12 16:01:53 -0500

    fixes to the interface

--- Project/MIOSimulation/MIOSimulation/Map.Designer.cs
+++ Project/MIOSimulation/MIOSimulation/Map.Designer.cs
@@ -34,6 +34,8 @@
             this.StationStop_CB = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.horaFinTxt = new System.Windows.Forms.TextBox();
             this.horaInicioTxt = new System.Windows.Forms.TextBox();
@@ -52,17 +54,15 @@
             this.prueba = new System.Windows.Forms.Label();
             this.startSimulation = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.listLines = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            //
+            // 
             // gmap
-            //
-            this.gmap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
+            // 
+            this.gmap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gmap.Bearing = 0F;
             this.gmap.CanDragMap = true;
@@ -90,9 +90,9 @@
             this.gmap.Zoom = 14D;
             this.gmap.OnMapZoomChanged += new GMap.NET.MapZoomChanged(this.addStationsOverlay);
             this.gmap.Load += new System.EventHandler(this.Gmap_Load_1);
-            //
+            // 
             // StationStop_CB
-            //
+            // 
             this.StationStop_CB.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StationStop_CB.FormattingEnabled = true;
             this.StationStop_CB.Location = new System.Drawing.Point(73, 53);
@@ -102,9 +102,9 @@
             this.StationStop_CB.TabIndex = 1;
             this.StationStop_CB.Text = "Estaciones y paradas";
             this.StationStop_CB.SelectedIndexChanged += new System.EventHandler(this.StationStop_CB_SelectedIndexChanged);
-            //
+            // 
             // label1
-            //
+            // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
@@ -114,15 +114,14 @@
             this.label1.Size = new System.Drawing.Size(89, 40);
             this.label1.TabIndex = 2;
             this.label1.Text = "Filtro";
-            //
+            // 
             // panel1
-            //
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Salmon;
-
-            this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.listLines);
+            this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.horaFinTxt);
@@ -150,9 +149,29 @@
             this.panel1.Size = new System.Drawing.Size(294, 863);
             this.panel1.TabIndex = 3;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
-            //
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(170, 361);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 20);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "dd-mm-yy hh:mm:ss ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(26, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(153, 20);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "dd-mm-yy hh:mm:ss ";
+            // 
             // label6
-            //
+            // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Black;
@@ -161,23 +180,23 @@
             this.label6.Size = new System.Drawing.Size(168, 40);
             this.label6.TabIndex = 19;
             this.label6.Text = "Simulacion";
-            //
+            // 
             // horaFinTxt
-            //
+            // 
             this.horaFinTxt.Location = new System.Drawing.Point(171, 341);
             this.horaFinTxt.Name = "horaFinTxt";
             this.horaFinTxt.Size = new System.Drawing.Size(100, 26);
             this.horaFinTxt.TabIndex = 18;
-            //
+            // 
             // horaInicioTxt
-            //
+            // 
             this.horaInicioTxt.Location = new System.Drawing.Point(27, 341);
             this.horaInicioTxt.Name = "horaInicioTxt";
             this.horaInicioTxt.Size = new System.Drawing.Size(100, 26);
             this.horaInicioTxt.TabIndex = 17;
-            //
+            // 
             // label5
-            //
+            // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
@@ -186,9 +205,9 @@
             this.label5.Size = new System.Drawing.Size(82, 28);
             this.label5.TabIndex = 16;
             this.label5.Text = "Hora fin";
-            //
+            // 
             // label4
-            //
+            // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
@@ -197,9 +216,9 @@
             this.label4.Size = new System.Drawing.Size(107, 28);
             this.label4.TabIndex = 15;
             this.label4.Text = "Hora inicio";
-            //
+            // 
             // zonesCheckedList
-            //
+            // 
             this.zonesCheckedList.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.zonesCheckedList.FormattingEnabled = true;
             this.zonesCheckedList.Items.AddRange(new object[] {
@@ -217,9 +236,9 @@
             this.zonesCheckedList.Size = new System.Drawing.Size(266, 134);
             this.zonesCheckedList.TabIndex = 14;
             this.zonesCheckedList.SelectedIndexChanged += new System.EventHandler(this.ZonesCheckedList_SelectedIndexChanged);
-            //
+            // 
             // label3
-            //
+            // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
@@ -228,9 +247,9 @@
             this.label3.Size = new System.Drawing.Size(196, 28);
             this.label3.TabIndex = 13;
             this.label3.Text = "Velocidad simulación";
-            //
+            // 
             // label2
-            //
+            // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
@@ -239,9 +258,9 @@
             this.label2.Size = new System.Drawing.Size(64, 28);
             this.label2.TabIndex = 12;
             this.label2.Text = "Zoom";
-            //
+            // 
             // trackBar1
-            //
+            // 
             this.trackBar1.LargeChange = 3;
             this.trackBar1.Location = new System.Drawing.Point(70, 782);
             this.trackBar1.Maximum = 18;
@@ -251,9 +270,9 @@
             this.trackBar1.TabIndex = 11;
             this.trackBar1.Value = 10;
             this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
-            //
+            // 
             // zoom
-            //
+            // 
             this.zoom.ForeColor = System.Drawing.Color.Black;
             this.zoom.Location = new System.Drawing.Point(46, 782);
             this.zoom.Name = "zoom";
@@ -262,9 +281,9 @@
             this.zoom.Text = "-";
             this.zoom.UseVisualStyleBackColor = true;
             this.zoom.Click += new System.EventHandler(this.Zoom_Click);
-            //
+            // 
             // zoomplus
-            //
+            // 
             this.zoomplus.ForeColor = System.Drawing.Color.Black;
             this.zoomplus.Location = new System.Drawing.Point(241, 782);
             this.zoomplus.Name = "zoomplus";
@@ -273,9 +292,9 @@
             this.zoomplus.Text = "+";
             this.zoomplus.UseVisualStyleBackColor = true;
             this.zoomplus.Click += new System.EventHandler(this.Zoomplus_Click);
-            //
+            // 
             // slower
-            //
+            // 
             this.slower.ForeColor = System.Drawing.Color.Black;
             this.slower.Location = new System.Drawing.Point(55, 540);
             this.slower.Name = "slower";
@@ -284,9 +303,9 @@
             this.slower.Text = "-";
             this.slower.UseVisualStyleBackColor = true;
             this.slower.Click += new System.EventHandler(this.Slower_Click);
-            //
+            // 
             // faster
-            //
+            // 
             this.faster.ForeColor = System.Drawing.Color.Black;
             this.faster.Location = new System.Drawing.Point(231, 543);
             this.faster.Name = "faster";
@@ -295,9 +314,9 @@
             this.faster.Text = "+";
             this.faster.UseVisualStyleBackColor = true;
             this.faster.Click += new System.EventHandler(this.Faster_Click);
-            //
+            // 
             // button2
-            //
+            // 
             this.button2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Black;
             this.button2.Location = new System.Drawing.Point(63, 479);
@@ -307,9 +326,9 @@
             this.button2.Text = "Pausar simulación";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
-            //
+            // 
             // goSimulation
-            //
+            // 
             this.goSimulation.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.goSimulation.ForeColor = System.Drawing.Color.Black;
             this.goSimulation.Location = new System.Drawing.Point(64, 443);
@@ -319,9 +338,9 @@
             this.goSimulation.Text = "Reanudar simulación";
             this.goSimulation.UseVisualStyleBackColor = true;
             this.goSimulation.Click += new System.EventHandler(this.GoSimulation_Click);
-            //
+            // 
             // prueba
-            //
+            // 
             this.prueba.AutoSize = true;
             this.prueba.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.prueba.ForeColor = System.Drawing.Color.Black;
@@ -330,9 +349,9 @@
             this.prueba.Size = new System.Drawing.Size(204, 28);
             this.prueba.TabIndex = 4;
             this.prueba.Text = "Informacion de la ruta";
-            //
+            // 
             // startSimulation
-            //
+            // 
             this.startSimulation.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startSimulation.ForeColor = System.Drawing.Color.Black;
             this.startSimulation.Location = new System.Drawing.Point(104, 377);
@@ -342,34 +361,22 @@
             this.startSimulation.Text = "Simular movimiento";
             this.startSimulation.UseVisualStyleBackColor = true;
             this.startSimulation.Click += new System.EventHandler(this.StartSimulation_Click);
-            //
+            // 
             // timer1
-            //
+            // 
             this.timer1.Interval = 300;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
-            //
-            // label7
-            //
-            this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(26, 361);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(101, 13);
-            this.label7.TabIndex = 20;
-            this.label7.Text = "dd-mm-yy hh:mm:ss ";
-            //
-            // label8
-            //
-            this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.Black;
-            this.label8.Location = new System.Drawing.Point(170, 361);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(101, 13);
-            this.label8.TabIndex = 21;
-            this.label8.Text = "dd-mm-yy hh:mm:ss ";
-            //
+            // 
+            // listLines
+            // 
+            this.listLines.FormattingEnabled = true;
+            this.listLines.Location = new System.Drawing.Point(102, 656);
+            this.listLines.Name = "listLines";
+            this.listLines.Size = new System.Drawing.Size(121, 28);
+            this.listLines.TabIndex = 22;
+            // 
             // SimulacionMetroCali
-            //
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1674, 860);
@@ -414,5 +421,6 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox listLines;
     }
 }
